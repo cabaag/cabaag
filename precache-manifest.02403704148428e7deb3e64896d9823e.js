50bc6efe1cea602074856237011bbaf5fdff91ee
cabaag <caba9313@gmail.com>
2019-12-15 00:29:58 -0600

    Updates

---
+++ precache-manifest.02403704148428e7deb3e64896d9823e.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8f84c9a605ff2f3889f036e6dae8522",
+    "url": "/cabaag/index.html"
+  },
+  {
+    "revision": "8ff81470582f9598cbd8",
+    "url": "/cabaag/static/css/2.83ace7dd.chunk.css"
+  },
+  {
+    "revision": "c3befe28ed73e018dcac",
+    "url": "/cabaag/static/css/main.fa11e6f4.chunk.css"
+  },
+  {
+    "revision": "8ff81470582f9598cbd8",
+    "url": "/cabaag/static/js/2.284c7e47.chunk.js"
+  },
+  {
+    "revision": "c3befe28ed73e018dcac",
+    "url": "/cabaag/static/js/main.efbc2444.chunk.js"
+  },
+  {
+    "revision": "fda05d8fb4dec64ad77b",
+    "url": "/cabaag/static/js/runtime-main.1ecebb36.js"
+  },
+  {
+    "revision": "4fdb1247ed69fc16adc7cbc321147ee5",
+    "url": "/cabaag/static/media/CV_ingles.4fdb1247.pdf"
+  },
+  {
+    "revision": "f3a08dafa70aff9f35deb4a698643538",
+    "url": "/cabaag/static/media/about.f3a08daf.png"
+  },
+  {
+    "revision": "14ed07e698c69df6da8744861c0ef7cd",
+    "url": "/cabaag/static/media/android.14ed07e6.svg"
+  },
+  {
+    "revision": "d5566a0700296c7353b4370f7170a12a",
+    "url": "/cabaag/static/media/angular.d5566a07.svg"
+  },
+  {
+    "revision": "4d3eb39eec4c2b00cc8183c9ae322d96",
+    "url": "/cabaag/static/media/apple.4d3eb39e.svg"
+  },
+  {
+    "revision": "b62fd8079b2613ac5d65ff9341d5def2",
+    "url": "/cabaag/static/media/express.b62fd807.png"
+  },
+  {
+    "revision": "6fddeffdbc020c13059e29362ead4b3e",
+    "url": "/cabaag/static/media/facebook.6fddeffd.svg"
+  },
+  {
+    "revision": "7f8d489c1887554fcf61932505d46393",
+    "url": "/cabaag/static/media/firebase.7f8d489c.svg"
+  },
+  {
+    "revision": "4870bd861df49ef53f29adffd6552a2b",
+    "url": "/cabaag/static/media/googleMaps.4870bd86.svg"
+  },
+  {
+    "revision": "f1e1679f78379912b24c2563b915fb21",
+    "url": "/cabaag/static/media/ionic.f1e1679f.svg"
+  },
+  {
+    "revision": "61ed8b054178d0e8d1285ad8f29f4045",
+    "url": "/cabaag/static/media/javascript.61ed8b05.svg"
+  },
+  {
+    "revision": "3bc4291e581bd6ef94e0fec5f499135c",
+    "url": "/cabaag/static/media/jquery.3bc4291e.svg"
+  },
+  {
+    "revision": "21203d1aea45e0da2a73f7dd4ff91dd5",
+    "url": "/cabaag/static/media/laravel.21203d1a.svg"
+  },
+  {
+    "revision": "e2d90de53a078968237f4b4b7b6081aa",
+    "url": "/cabaag/static/media/learning.e2d90de5.png"
+  },
+  {
+    "revision": "0ce2bfac64ad5a4366dab85d274b71e5",
+    "url": "/cabaag/static/media/mongodb.0ce2bfac.svg"
+  },
+  {
+    "revision": "b337a2fb0b1289e6757e632dc766ff6b",
+    "url": "/cabaag/static/media/nodejs.b337a2fb.svg"
+  },
+  {
+    "revision": "550b0103e59d03237dc28efcd9576a0a",
+    "url": "/cabaag/static/media/passport.550b0103.svg"
+  },
+  {
+    "revision": "ebc3a88ea568e7a5cba626174eef62bc",
+    "url": "/cabaag/static/media/paypal.ebc3a88e.svg"
+  },
+  {
+    "revision": "63f8625f213659f975b7017dc3b06351",
+    "url": "/cabaag/static/media/prestashop.63f8625f.svg"
+  },
+  {
+    "revision": "146716bdf1a351db5705ec1e6a25ed08",
+    "url": "/cabaag/static/media/python.146716bd.svg"
+  },
+  {
+    "revision": "1a36cc4d7f1b283edce389cb41bddb1f",
+    "url": "/cabaag/static/media/react.1a36cc4d.svg"
+  },
+  {
+    "revision": "1a36cc4d7f1b283edce389cb41bddb1f",
+    "url": "/cabaag/static/media/reactNative.1a36cc4d.svg"
+  },
+  {
+    "revision": "2723d3bd0e29e3a4f4744ea48ff2d915",
+    "url": "/cabaag/static/media/reactivex.2723d3bd.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/cabaag/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "b41ff58f6f76bdbd853213cdb6b320cf",
+    "url": "/cabaag/static/media/sass.b41ff58f.svg"
+  },
+  {
+    "revision": "3de182d2120823fe49c27eeb1765c53a",
+    "url": "/cabaag/static/media/typescript.3de182d2.svg"
+  },
+  {
+    "revision": "69cedcf3d2b148bdce953a480caa57b1",
+    "url": "/cabaag/static/media/vue-js.69cedcf3.svg"
+  },
+  {
+    "revision": "5f5a3b21ad4723ee4e2fd7761db1d01a",
+    "url": "/cabaag/static/media/webpack.5f5a3b21.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dccfedf8d77992614517060e966ed56c.js"
+  "/cabaag/precache-manifest.02403704148428e7deb3e64896d9823e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cabaag/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
